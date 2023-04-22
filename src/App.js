0ea66b3329f8d9e0d81d4c904edfb5f1d0652d3e
Danielxburns <danielburnstech@gmail.com>
2023-04-22 12:20:45 -0500

    change name to 'Daniel's Color Picker'

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import Picker from './components/picker.component';
 import { ImagesProvider } from './contexts/images.context';
 
 function App() {
-
   return (
     <div className="App">
       <p>Daniel's Color Tool</p>
       <div className="picker-container">
         <ImagesProvider>
-          <Picker colorModel={'RGBA'}/>
+          <Picker colorModel={'RGBA'} />
         </ImagesProvider>
         <ImagesProvider>
-          <Picker colorModel={'HSLA'}/>
+          <Picker colorModel={'HSLA'} />
         </ImagesProvider>
       </div>
     </div>
